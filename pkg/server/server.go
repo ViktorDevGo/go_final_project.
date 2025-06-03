d4ca389736d80fe164fd6b8ae3b90ff268b0d33e
Viktor <v.boyarkin@slata.com>
2025-06-03 10:08:23 +0800

    step_1

---
+++ pkg/server/server.go
@@ -0,0 +1,20 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	nextdate "github.com/ViktorDevGo/go_final_project/pkg/api"
+)
+
+func Run() error {
+	//nextdate.Init()
+	newr := nextdate.Init()
+	port := 7540
+	fmt.Println("Запуск сервера на порту %d\n", port)
+
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), newr)
+	//http.Handle("/", http.FileServer(http.Dir("web")))
+	//fmt.Println("WWWWWW")
+	//return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+}
